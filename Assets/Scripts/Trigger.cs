9ca0e225c4315083894348e957eee24e5aeb5bfc
Senthuran12 <148048451+Senthuran12@users.noreply.github.com>
2025-10-04 09:12:05 +0100

    Testing triggers still working on it

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] bool destroyOnTriggerEnter;
+    [SerializeField] string tagFilter;
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Has entered");
+        if (!string.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerEnter.Invoke();
+        if (destroyOnTriggerEnter)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (!string.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerExit.Invoke();
+    }
+}
